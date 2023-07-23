b09ccb4619e30fe173fe27465975e3d52b4944d4
min-young417 <nonamed1021@naver.com>
2023-07-23 14:26:29 +0900

    HW#4

---
+++ w4_HW/1972.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+import javax.print.attribute.standard.PDLOverrideSupported;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num == 0) {
+				if(pq.isEmpty()) sb.append("0\n");
+				else sb.append(pq.poll()).append('\n');
+			}else pq.add(num);
+		}
+		System.out.println(sb);
+	}
+}
