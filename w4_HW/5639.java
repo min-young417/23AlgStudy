c9826c07c4735fc2d13366456f2ba2635dd76530
min-young417 <nonamed1021@naver.com>
2023-07-26 22:10:55 +0900

    HW#4

---
+++ w4_HW/5639.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        void insert(int n) {
+            if (n < this.data) {
+                if (this.left == null)
+                    this.left = new Node(n);
+                else this.left.insert(n);
+            } else {
+                if (this.right == null)
+                    this.right = new Node(n);
+                else this.right.insert(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Node root = new Node(Integer.parseInt(br.readLine()));
+        String input;
+        while (true) {
+            input = br.readLine();
+            if (input == null || input.equals(""))
+                break;
+            root.insert(Integer.parseInt(input));
+        }
+
+        postOrder(root);
+    }
+
+    static void postOrder(Node node) {
+        if (node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.data);
+    }
+}
